5c7faa4636c540c257b8128c1d562b562a67ada0
boham97 <kangbosung97@gmail.com>
2022-07-29 22:39:54 +0900

    0729 solve 17070.py

--- 17070.py
+++ 17070.py
@@ -2,41 +2,22 @@
 n = int(input())
 
 mat = [[] for i in range(n)]
+result_mat = [[[0,0,0,] for j in range(n)] for i in range(n)]
 for i in range(n):
     mat[i]= list(map(int,input().split()))
+result_mat[0][1][0] =1
+for i in range(2,n):
+    if mat[0][i-1] ==0 and mat[0][i] == 0:
+        result_mat[0][i][0] = result_mat[0][i-1][0]
+for i in range(1,n):
+    for j in range(2,n):
+        if mat[i][j-1] == 0 and mat[i][j] ==0:
+            result_mat[i][j][0] = result_mat[i][j][0] + result_mat[i][j-1][0] + result_mat[i][j-1][2]
+        if mat[i][j-1] ==0 and mat[i][j] ==0 and mat[i-1][j] == 0:
+            result_mat[i][j][2] = result_mat[i][j][2] + result_mat[i-1][j-1][0] +result_mat[i-1][j-1][1] + result_mat[i-1][j-1][2]
+        if mat[i-1][j] == 0 and mat[i][j] == 0:
+            result_mat[i][j][1] =result_mat[i][j][1] + result_mat[i-1][j][2] + result_mat[i-1][j][1]
 
-for i in range(1, n-1):
-    A, B, C = n-i-2, n-i-1, i
-    Blist = [0 for j in range(i + 1)]
-    for j in range((B+1)**(i-1) *B + 1):
-        save = []
-        while(j > B):
-            j, temp = divmod(j, n-i)
-            save.insert(0, temp)
-        save.insert(0, j)
-        if sum(list(map(int,list(save)))) == B:
-            temp2 = len(save)
-            for k in range(i-temp2+1):
-                save.insert(0, 0)
-            #print(save)
-            Blist = save[:]
-            nonB = []
-            lenA = 0
-            for k in range(len(Blist)):
-                if Blist[k] ==0:
-                    nonB.append(k)
-                    lenA += 1
-            
-            for k in range((A+1)**(lenA-1) * A+1):
-                Alist = [0 for j in range(i + 1)]
-                save = ''
-                #print(k ,A)
-                while(k > A):
-                    k, temp = divmod(k, A+1)
-                    save = str(temp) + save
-                save =  str(k) + save
-                if sum(list(map(int,list(save)))) == A:
-                    for m in range(len(save)):
-                        Alist[nonB[len(nonB)-1-m]] = int(save[len(save)-1-m])
-                    print(Alist, Blist)
-                    
+
+
+print(sum(result_mat[n-1][n-1]))
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 백준 sw test A 기출
-### 16637 괄호 추가하기
+
+### [16637 괄호 추가하기](https://www.acmicpc.net/problem/16637)
+
+### [17070 파이프 옮기기](https://www.acmicpc.net/problem/17070)

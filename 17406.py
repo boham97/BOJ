9129b76491b308ffd963e186e1be6e6b699946fd
boham97 <kangbosung97@gmail.com>
2022-08-02 23:06:18 +0900

    solve17406

--- 17406.py
+++ 17406.py
@@ -8,8 +8,6 @@ for i in range(x):
     first_mat[i] = list(map(int,input().split()))
 
 out_mat = [[] for i in range(x)]
-for i in range(x):
-    out_mat[i] = first_mat[i][:]
 
 turn_list = [[0,0,0] for i in range(z)]
 turn_mat = [0 for i in range(z)]
@@ -20,42 +18,27 @@ for i in range(z):
     turn_list[i][2] = s
 result_mat = inf
 for i in itertools.permutations(range(z),z):
-    for n in range(z+1):
-        for p in list(itertools.combinations(range(z),n)):
-            for m in range(x):
-                out_mat[m] = first_mat[m][:]
-            for m in i:
-                #print(i,n,p,m)
-                if m not in p:
-                    #print(turn_list[m])
-                    r, c, s = turn_list[m]
-                    for j in range(1, s + 1):
-                        temp =out_mat[r -j -1][c -j -1]
-                        for k in range(2*j):
-                            out_mat[r-j-1+k][c-j-1] = out_mat[r-j-1+k+1][c-j-1]
-                        for k in range(2*j):
-                            out_mat[r+j-1][c-j-1+k] = out_mat[r+j-1][c-j-1+k+1]
-                        for k in range(2*j):
-                            out_mat[r+j-1-k][c+j-1] = out_mat[r+j-1-k -1][c+j-1]
-                        for k in range(2*j):
-                            out_mat[r-j-1][c+j-1-k] = out_mat[r-j-1][c+j-1-k-1]
-                        out_mat[r-j-1][c-j] = temp
-                else:
-                    r, c, s = turn_list[m]
-                    for j in range(1, s + 1):
-                        temp =out_mat[r -j -1][c -j -1]
-                        for k in range(2*j):
-                            out_mat[r-j-1][c-j-1+k] = out_mat[r-j-1][c-j-1+k+1]
-                        for k in range(2*j):
-                            out_mat[r-j-1+k][c+j-1] = out_mat[r-j-1+k+1][c+j-1]
-                        for k in range(2*j):
-                            out_mat[r+j-1][c+j-1-k] = out_mat[r+j-1][c+j-1-k-1]
-                        for k in range(2*j):
-                            out_mat[r+j-1-k][c-j-1] = out_mat[r+j-1-k-1][c-j-1]
-                    out_mat[r-j-1][c-j] = temp
-            for z1 in range(x):
-                temp2 =sum(out_mat[z1])
-            if temp2 < result_mat:
-                result_mat = temp2
+    for m in range(x):
+        out_mat[m] = first_mat[m][:]
+    for m in i:
+        #print(i,m)
+        r, c, s = turn_list[m]
+        for j in range(1, s + 1):
+            temp =out_mat[r -j -1][c -j -1]
+            for k in range(2*j):
+                out_mat[r-j-1+k][c-j-1] = out_mat[r-j-1+k+1][c-j-1]
+            for k in range(2*j):
+                out_mat[r+j-1][c-j-1+k] = out_mat[r+j-1][c-j-1+k+1]
+            for k in range(2*j):
+                out_mat[r+j-1-k][c+j-1] = out_mat[r+j-1-k -1][c+j-1]
+            for k in range(2*j):
+                out_mat[r-j-1][c+j-1-k] = out_mat[r-j-1][c+j-1-k-1]
+            out_mat[r-j-1][c-j] = temp
+        temp2 = []
+    for z1 in range(x):
+        temp2.append(sum(out_mat[z1]))
+    #print(min(temp2))
+    if min(temp2) < result_mat:
+        result_mat = min(temp2)
                 #print(result_mat)
 print(result_mat)    
--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 ### [야구](https://www.acmicpc.net/problem/17281)
 
 - 순열을 만들때 최대한 간단한 형태로 만들자
+- 느리면 pypy로 
+
+### [17472다리만들기2](https://www.acmicpc.net/problem/17472)
+
+### [배열돌리기4](https://www.acmicpc.net/problem/17406)

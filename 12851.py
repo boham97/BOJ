18f888bc7169875b0751415bc596c39f027ffc4f
boham97 <kangbosung97@gmail.com>
2022-10-09 16:24:08 +0900

    1009

--- 12851.py
+++ 12851.py
@@ -1,43 +1,12 @@
 from collections import deque
 
 
-<<<<<<< HEAD
-arr =[100001] * 100001
-=======
 arr =[100000] * 100001
 time = [0] * 100001
->>>>>>> ba140444107e26e3fa75a12b44a4e8e936c7c0ee
 N, K = map(int, input().split())
 arr[N] = 0
 que = deque()
 que.append(N)
-<<<<<<< HEAD
-ans = 1
-while que:
-    now = que.popleft()
-    for num in [now-1,now+1,now*2]:
-        if 0<= num < 100001 and arr[num] > arr[now]+ 1:
-                que.append(num)
-                arr[num] = arr[now]+ 1
-        if num == K and arr[K] == arr[now] + 1:
-            ans += 1
-print(arr[K])
-print(ans)
-
-
-"""     if now-1 >= 0 and arr[0][now-1] > arr[0][now]+1:
-        que.append(now-1)
-        arr[0][now-1] = arr[0][now]+1
-    if now+1 < 100001 and arr[0][now+1] > arr[0][now]+1:
-        que.append(now+1)
-        arr[0][now+1] = arr[0][now]+1
-    if now*2 < 100001 and arr[0][now*2] > arr[0][now]:
-        que.append(now*2)
-        arr[0][now*2] = arr[0][now]
-    if arr[K] != 100000:
-        break
-print(arr[K]) """
-=======
 ans = 0
 while que:
     now = que.popleft()
@@ -59,4 +28,3 @@ if N != K:
     print(ans)
 else:
     print(1)
->>>>>>> ba140444107e26e3fa75a12b44a4e8e936c7c0ee
--- 1753.py
+++ 1753.py
@@ -6,7 +6,6 @@ K=int(sys.stdin.readline())
 dp = [1e9]*(V+1)
 dp[K] = 0
 graph = [[] for _ in range(V+1)]
-<<<<<<< HEAD
 heap = []
 heapq.heappush(heap,(0,K))
 
@@ -28,60 +27,3 @@ for x in dp[1:]:
         print(x)
     else:
         print('INF')
-=======
-for i in range(E):
-    u,v,w = map(int,sys.stdin.readline().split())
-    graph[u].append((v,w))
-INF = 1e9
-d = [INF] * (V+1)
-d[start] = 0
-visited = [0]*(V+1)
-visited[start] = 1
-<<<<<<< HEAD
-for j,k in graph[start]:
-    d[j] = min(d[j],k)
-for i in range(1,V+1):
-    graph[i].sort(key = lambda x:x[1],reverse=True)
-for _ in range(V):
-    temp_min = INF
-    next = 0
-    for i in range(V):
-        if visited[i] == 1:
-            continue
-        for j in range(len(graph[i])-1,-1,-1):
-            if visited[graph[i][j][0]] == 0 and graph[i][j][1]<temp_min:
-                temp_min = graph[i][j][1]
-                next = graph[i][j][0]
-    visited[next] = 1
-    for i in range(len(graph[next])):
-        d[graph[next][i][0]] = min(d[graph[next][i][0]], d[next]+graph[next][i][1])
-=======
-used =[start]
-for j,k in graph[start]:
-    d[j] = min(d[j],k)
-
-for _ in range(V):
-    temp_min = INF
-    next = 0
-    for i in used:
-        for j,k in graph[i]:
-            if visited[j] == 0 and k<temp_min:
-                temp_min = k
-                next = j
-    visited[next] = 1
-    used.append(next)
-    for i,j in graph[next]:
-        d[i] = min(d[i], d[next]+j)
-    #print(d)
->>>>>>> 9ab4d46fca30cf41210c3fefd9a626b9f9d66caf
-for i in range(1,V+1):
-    if d[i] == INF:
-        print('INF')
-    else:
-<<<<<<< HEAD
-        print(d[i])
-print(graph)
-=======
-        print(d[i])
->>>>>>> 9ab4d46fca30cf41210c3fefd9a626b9f9d66caf
->>>>>>> ba140444107e26e3fa75a12b44a4e8e936c7c0ee

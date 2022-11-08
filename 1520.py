212c70a384677045e52f8b5798fbd99e3e7ec67b
boham97 <kangbosung97@gmail.com>
2022-11-08 17:08:31 +0900

    내리막길

--- 1520.py
+++ 1520.py
@@ -1,39 +1,38 @@
-
-dlt = [[-1,0],[0,1], [0, -1],[1,0]]
-N, M = map(int,input().split())
+dlt = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+N, M = map(int, input().split())
 cnt = 0
 arr = [list(map(int, input().split())) for _ in range(N)]
 DP = [[0] * M for _ in range(N)]
-DP[0][0] = 1
-visited = [[[0]*M for _ in range(N)]]
-visited[0][0] = 1
-stack = [[0,0,-1]]
-nodrt = -1
+DP[N-1][M-1] = 1
+stack = [[0, 0, -1]]
+flag = 0
 while stack:
-    x, y,drt = stack[-1]
-    if x== N-1 and y==M-1:
-        cnt += 1
-        print(stack)
-        for x,y,z in stack:
-            DP[x][y] = 1
-    for i in range(4):
-        dx, dy = x+dlt[i][0], y+dlt[i][1]
-        if 0<= dx < N and 0<= dy < M and arr[x][y] > arr[dx][dy] and i != nodrt:
-            if DP[dx][dy] == 0 and visited[dx][dy] == 0:
-                visited[dx][dy] = 1
-                stack.append([dx,dy,i])
-                nodrt = -1
-                break
-            elif DP[dx][dy] != 0 and visited[dx][dy]== 1:
-                for x,y,z in stack:
-                    DP[x][y] += DP[dx][dy] 
-                print(stack)
-                print(nodrt, i)
-                cnt += DP[dx][dy]
-                DP[dx][dy] +=1
-            
+    x, y, z = stack[-1]
+    if flag:
+        flag = 0
+        z = temp
     else:
-        buffer1, buffer2, nodrt = stack.pop()
-print(DP)
-print(visited)
+        z = -1
+    for i in range(z+1, 4):
+        dx, dy = x+dlt[i][0], y+dlt[i][1]
+        if 0 <= dx < N and 0 <= dy < M and arr[x][y] > arr[dx][dy] and DP[dx][dy] != -1:
+            if DP[dx][dy] == 0:
+                stack.append([dx, dy, i])
+                break
+            if DP[dx][dy] > 0:
+                for x, y, z in stack:
+                    DP[x][y] += DP[dx][dy]
+                #print(x, y, stack)
+                cnt += DP[dx][dy]
+    else:
+        flag = 1
+        if DP[x][y] == 0:
+            DP[x][y] = -1
+        temp1, temp2, temp = stack.pop()
+#print(DP)
 print(cnt)
+
+'''
+못가는 길이면 pop 했을떄 DP[x][y] == 0 => DP[x][y] = -1 로 변경
+-1이면 안가서 경우의수 줄이기
+'''

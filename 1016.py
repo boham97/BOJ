ef60da4338d872776e8237938fae2413be7c9560
boham97 <kangbosung97@gmail.com>
2023-03-20 23:08:53 +0900

    제곱 ㄴㄴ수

--- 1016.py
+++ 1016.py
@@ -1,10 +1,11 @@
 n, m = map(int, input().split())
 
 arr = [0] * (m - n + 1)
-for i in range(2, 1001001):
-    if arr[i] == 0:
-        j = 1
+for i in range(2, int(m**0.5) + 1):
+        j = n// i** 2
         while i ** 2 * j <= m:
-            arr[i ** 2 * j] = 1
+            if i ** 2 * j >= n and arr[i ** 2 * j - n] == 0:
+                arr[i ** 2 * j - n] = 1
+            
             j += 1
-print(m-n-sum(arr[n:m+1])+1)
+print(m-n-sum(arr) + 1)

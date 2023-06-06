7b751ddcd6a9307e7f9ab1a25a873aa2657fa060
boham97 <kangbosung97@gmail.com>
2023-06-06 13:53:27 +0900

    치즈 dfs+bfs 혼합

--- 2638.py
+++ 2638.py
@@ -1,49 +1,40 @@
 import sys
 from collections import deque
-import heapq
-from pprint import pprint
-
-delx = [-1, 1, 0, 0]
-dely = [0, 0 ,1, -1]
-
 
 input = sys.stdin.readline
 
+dx = [-1, 1, 0 , 0]
+dy = [0, 0, 1, -1]
 N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+visit = [[0] *M for _ in range(N)]
+ans = [0] * 200
+que = deque()
+que.append((0,0))
+visit[0][0] = 1
 
-arr = list(list(map(int,input().split())) for _ in range(N))
+while que:
+    i, j = que.popleft()
+    for k in range(4):
+        x = i+ dx[k]
+        y = j + dy[k]
+        if 0<= x < N and 0 <= y < M and visit[x][y] == 0:
+            if arr[x][y] == 0:
+                visit[x][y] = visit[i][j]
+                que.appendleft((x,y))
+            elif arr[x][y] == 1:
+                temp = 0
+                for l in range(4):
+                    x2 = x+ dx[l]
+                    y2 = y + dy[l]
+                    if 0<= x2 < N and 0 <= y2 < M and 0 < visit[x2][y2] <= visit[i][j]:
+                        temp += 1
+                if temp > 1:
+                    visit[x][y] = visit[i][j] + 1
+                    que.append((x,y))
+                    ans[visit[x][y]] += 1
 
-
-
-air = deque()
-air.append((0,0))
-arr[0][0] = 2
-
-while air:
-    x, y = air.popleft()
-    
-
-    for i in range(4):
-        dx = x + delx[i]
-        dy = y + dely[i]
-        if 0 <= dx < N and 0 <= dy < M:
-            if arr[dx][dy] == 0:
-                air.appendleft((dx,dy))
-                arr[dx][dy] = arr[x][y]
-            elif arr[dx][dy] == 1:
-                logic = 0
-                for i in range(4):
-                    dx2 = dx + delx[i]
-                    dy2 = dy + dely[i]
-                    if 0 <= dx2 < N and 0 <= dy2 < M and 1< arr[dx2][dy2] <= arr[x][y]:
-                        logic += 1
-                if logic > 1:
-                    arr[dx][dy] = arr[x][y] + 1
-                    air.append((dx,dy))
-
-
-ans = 0
-for i in range(N):
-    ans = max(max(arr[i]), ans)
-
-print(ans-2)
+for i in range(199, 0, -1):
+    if ans[i] != 0:
+        print(i - 1)
+        break

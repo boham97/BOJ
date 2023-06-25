eaec0934aca2cf3d58b7b33713c4b2b876eba04f
boham97 <kangbosung97@gmail.com>
2023-06-25 12:33:23 +0900

    틀린 문제 수정 index error

--- 9935.py
+++ 9935.py
@@ -1,24 +1,15 @@
-import sys
-
-input = sys.stdin.readline
-
-arr = list(input())[:-1]
-temp = list(input())[:-1]
+arr = list(input())
+temp = list(input())
 collect = len(temp)
-
-bomb = {}
-
-for i in range(collect):
-    bomb[temp[i]] = i
 
 ans = []
 
 for i in range(len(arr)):
 
     ans.append(arr[i])
-    if arr[i] == temp[-1] and i + 1 >= collect:
+    if arr[i] == temp[-1] and len(ans) >= collect:
         for j in range(collect):
-            if ans[(-1-j)] != temp[(-1-j)]:
+            if ans[(-1-j)] != temp[(-1-j)] : 
                 break
         else:
             for _ in range(collect):

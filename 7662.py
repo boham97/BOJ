a7955e1be9b015370df371866803bf5a4c07204b
boham97 <kangbosung97@gmail.com>
2023-07-02 17:59:48 +0900

    이중우선순위 큐 heapq와 visited

---
+++ 7662.py
@@ -0,0 +1,39 @@
+from heapq import heappush, heappop
+
+T = int(input())
+for _ in range(T):
+    k = int(input())
+
+    arr = [0] *(k)
+    iq = []
+    dq = []
+
+    for i in range(k):
+        a, s =  input().split()
+        s = int(s)
+        if a == 'I':
+            heappush(iq, (-s, i))
+            heappush(dq, (s,i))
+        else:
+            if s == 1:
+                while iq and arr[iq[0][1]]:
+                    heappop(iq)
+                if iq and not arr[iq[0][1]]:
+                    arr[iq[0][1]] = 1
+                    heappop(iq)
+            else:
+                while dq and arr[dq[0][1]]:
+                    heappop(dq)
+                if iq and not arr[iq[0][1]]:
+                    arr[dq[0][1]] = 1
+                    heappop(dq)
+                    
+    while iq and arr[iq[0][1]]:
+        heappop(iq)
+    while dq and arr[dq[0][1]]:
+        heappop(dq)
+
+    if iq:
+        print(-iq[0][0], dq[0][0])
+    else:
+        print('EMPTY')

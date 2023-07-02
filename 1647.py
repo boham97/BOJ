f7734fccd072d68cdc7d79d017ab5b2ed3d3ac30
boham97 <kangbosung97@gmail.com>
2023-07-02 18:49:44 +0900

    도시 분할 계확 최소신장 트리

---
+++ 1647.py
@@ -0,0 +1,38 @@
+from heapq import heappush, heappop
+import sys
+input = sys.stdin.readline
+
+def find(node):
+    if parent[node] != node:
+        return find(parent[node])
+    else:
+        return node
+
+
+def union(a, b):
+    if a > b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+V, E = map(int, input().split())
+parent = [i  for i in range(V+1)]
+
+arr =[]
+for _ in range(E):
+    arr.append(list(map(int, input().split())))
+arr.sort( key= lambda x : x[2])
+
+ans = 0
+last = 0
+for i in range(E):
+    a, b, c = arr[i]
+    pa, pb = find(a), find(b)
+    if pa != pb:
+        union(pa,pb)
+        last = c
+        ans += c
+
+
+print(ans-last)

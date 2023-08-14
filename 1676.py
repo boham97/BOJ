9c83e1d792d3efac668f1b428a7977185e9529af
boham97 <kangbosung97@gmail.com>
2023-08-14 23:47:35 +0900

    수학

---
+++ 1676.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+two = 0
+five = 0
+for i in range(1,N+1):
+    while i%2 ==0:
+        two += 1
+        i //= 2
+    
+    while i%5 ==0:
+        five += 1
+        i //= 5
+        
+print(min(two,five))

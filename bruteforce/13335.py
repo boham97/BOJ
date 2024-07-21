37f29ea7c60568b8828003f517d24c2062cced34
boham97 <kangbosung97@gmail.com>
2024-07-21 16:05:48 +0900

    트럭 큐

--- bruteforce/13335.py
+++ bruteforce/13335.py
@@ -1,4 +1,7 @@
 from collections import deque
+from sys import stdin
+
+ibput = stdin.readline
 
 N, W, L = map(int, input().split())
 trucks = list(map(int, input().split()))
@@ -9,14 +12,19 @@ que = deque()
 
 for truck in trucks:
     move = False
-    while on + truck > L or len(que)>= W:
+    while on + truck > L or len(que) >= W:
         t, created_at = que.popleft()
         on -= t
         time = created_at + W
+        if que:
+            time = max(time, que[-1][1] + 1)
         move = True
-        print("move")
     if not move:
         time += 1
     que.append((truck, time))
     on += truck
-    print(time, truck)
+    
+    
+print(time)
+
+

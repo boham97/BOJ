3936abb2c85742d69fefd5ce453028cfcb0d7934
boham97 <kangbosung97@gmail.com>
2024-10-22 22:11:36 +0900

    세제곱근 decimal

--- 3783.py
+++ 3783.py
@@ -24,5 +24,28 @@ t = int(input())
 for _ in range(t):
     ans = bs().quantize(Decimal('1.0000000000'), ROUND_DOWN)
     print(ans)
+######################################
+from decimal import Decimal, getcontext, ROUND_DOWN
+
+getcontext().prec = 1000
+    
+   
+def bs(n):
+    n = Decimal(n + '.0000000000')
+    pow = Decimal('1') / Decimal('3')
+    ans = Decimal(n ** pow)
+    ans = round(ans, 500)
+    ans = Decimal(ans).quantize(Decimal('.0000000001'), rounding=ROUND_DOWN)
+    return ans
+
+
+t = int(input())
+
+for _ in range(t):
+    ans = bs(input().rstrip())
+    print(ans)
+
+
+
 
 

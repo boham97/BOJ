abf627063cc92fc84e38bb45aa992b180db614ca
boham97 <kangbosung97@gmail.com>
2024-12-27 23:17:26 +0900

    하노이 탑 리팩토링

--- bruteforce/11729.py
+++ bruteforce/11729.py
@@ -1,11 +1,12 @@
-def hanoi(a, b, c, d):
+def hanoi(a, b, c):
     if a == 1:
         print(b, c)
     else:
-        hanoi(a - 1, b,d, c)
-        hanoi(1, b, c ,d)
-        hanoi(a - 1, d, c, b)
+        hanoi(a - 1, b, 6 - b - c)
+        hanoi(1, b, c)
+        hanoi(a - 1, 6 - b - c, c)
 
 n = int(input())
 print(2**n -1)
-hanoi(n, 1, 3, 2)
+if n <= 20:
+    hanoi(n, 1, 3)

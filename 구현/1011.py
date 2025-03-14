d033304b3b00c218a8e0a802dbb4041235d38f10
boham97 <kangbosung97@gmail.com>
2025-03-14 22:42:58 +0900

    멍멍이 쓰다듬기 수학

---
+++ 구현/1011.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+for _  in range(n):
+    x, y = map(int ,input().split())
+    t = y -x
+    ans = 0
+    i = 1
+    while t > 0:
+        if t <= i:
+            ans += 1
+            break
+        t -= 2*i
+        ans += 2
+        i += 1
+    print(ans)
